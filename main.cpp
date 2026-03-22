58a6f3b6116497c543675db00b029fffdcddbc19
zbon0830 <zacharybon0830@yahoo.com>
2026-03-22 15:10:04 -0400

    Created OrderedStore and UnorderedStore struct.

--- main.cpp
+++ main.cpp
@@ -12,5 +12,18 @@ struct Game {
     string app_id;
     string name;
     double price;
+    int priceCents; // convert 19.99 to 1999 for easier indexing
     string genres;
+};
+
+struct OrderedStore {
+    map<string, Game> gamesbyID;
+    map<int, vector<string>> priceIndex;
+    map<string, vector<string>> genreIndex;
+};
+
+struct UnorderedStore {
+    unordered_map<string, Game> gamesById;
+    unordered_map<int, vector<string>> priceIndex;
+    unordered_map<string, vector<string>> genreIndex;
 };
